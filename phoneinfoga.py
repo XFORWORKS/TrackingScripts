ab4f2e0301ecbb37452c4f1f84f86b1f5cdcab54
XFORWORKS <67760271+XFORWORKS@users.noreply.github.com>
2020-10-24 15:20:20 +0300

    Update phoneinfoga.py

--- phoneinfoga.py
+++ phoneinfoga.py
@@ -23,7 +23,7 @@ def banner():
     print(" \/    |_| |_|\___/|_| |_|\___\____/ |_| |_|_|  \___/ \__, |\__,_|")
     print("                                                      |___/       ")
     print(" PhoneInfoga Ver. {}".format(__version__))
-    print(" Coded by Charon IV")
+    print(" Coded by XFORWORKS")
     print("\n")
 
 banner()
